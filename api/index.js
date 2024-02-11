986fd816594ecb4f585232622a5f9d844287b347
Sohoom_418 <125085733+SohoomlalBanerjee@users.noreply.github.com>
2024-02-11 21:14:10 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: 'http://localhost:5174',
+  origin: 'http://localhost:5173',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204,
